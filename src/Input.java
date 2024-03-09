2a865bf6b32c249f19adce752dc0d1d0c404f08b
wathmal96 <ranaweera1125vinura@gmail.com>
2024-03-09 13:46:17 +0530

    3rd commit

--- src/Input.java
+++ src/Input.java
@@ -3,6 +3,7 @@ public class Input {
     public static int getData(String inputName){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Number for "+inputName+":");
+        System.out.println(" ");
         return scanner.nextInt();
     }
 }
